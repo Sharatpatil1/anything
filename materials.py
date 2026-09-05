6b46c712d97a11b8520d68b80ccac8aaf8b6be25
vaishh456 <vaishhkuppe@gmail.com>
2026-09-05 23:30:18 +0530

    Add Member 5 project files

---
+++ materials.py
@@ -0,0 +1,30 @@
+materials = {
+
+    "Stone": {
+        "thermal_conductivity": 1.7,
+        "density": 2600,
+        "specific_heat": 790,
+        "cost": 1200
+    },
+
+    "Concrete": {
+        "thermal_conductivity": 1.4,
+        "density": 2400,
+        "specific_heat": 880,
+        "cost": 1000
+    },
+
+    "Brick": {
+        "thermal_conductivity": 0.72,
+        "density": 1800,
+        "specific_heat": 840,
+        "cost": 800
+    },
+
+    "Composite": {
+        "thermal_conductivity": 0.35,
+        "density": 1200,
+        "specific_heat": 1000,
+        "cost": 1500
+    }
+}
